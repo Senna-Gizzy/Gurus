64131f17d47113ef2a52a6171f7e2f83d7fc087c
Senna-Gizzy <sennabeetje@gmail.com>
2026-03-09 12:30:08 +0100

    Update home.py

--- home.py
+++ home.py
@@ -1,32 +1,47 @@
 import streamlit as st
-import pandas as pd
 
 st.title("Guru's Only")
 
 CORRECT_PASSWORD = st.secrets["guru_password"]
 
+sheet_link_data = "https://docs.google.com/spreadsheets/d/1Lf9Rs121pEpCPjAhEC7pk00ffvmOim51vLmrUePJP5A/edit#gid=0"
+sheet_link_edit = "https://docs.google.com/spreadsheets/d/10kSJ04j7sNsXuXEinq5sVI_ToM13g8RroU0lSbmic0Y/edit?gid=0#gid=0"
 
-password = st.text_input("Wachtwoord", type="password")
-if st.button("Login"):
-    if password == CORRECT_PASSWORD:
-        st.session_state.authenticated = True
-        sheet_link_data = "https://docs.google.com/spreadsheets/d/1Lf9Rs121pEpCPjAhEC7pk00ffvmOim51vLmrUePJP5A/edit#gid=0"
-        sheet_link_edit = "https://docs.google.com/spreadsheets/d/10kSJ04j7sNsXuXEinq5sVI_ToM13g8RroU0lSbmic0Y/edit?gid=0#gid=0"
-        st.markdown(f"""
-        <a href="{sheet_link_data}" target="_blank">
-            <button style="padding:10px 20px; font-size:14px;">Naar Gamelibrary</button>
-        </a>
-        """, unsafe_allow_html=True)
+if "authenticated" not in st.session_state:
+    st.session_state.authenticated = False
 
-        st.markdown(f"""
-        <a href="{sheet_link_edit}" target="_blank">
-            <button style="padding:10px 20px; font-size:14px;">Spel Opmerking Toevoegen</button>
-        </a>
-        """, unsafe_allow_html=True)
-        
-        st.divider()
-    else:
-        st.error("Verkeerd wachtwoord")
+if "login_clicked" not in st.session_state:
+    st.session_state.login_clicked = False
 
-# Stop at the end to prevent the app from executing further
-st.stop()
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("Naar Gamelibrary"):
+        st.session_state.login_clicked = True
+
+with col2:
+    st.markdown(f"""
+    <a href="{sheet_link_edit}" target="_blank">
+        <button style="padding:10px 20px; font-size:14px;">Spel Opmerking Toevoegen</button>
+    </a>
+    """, unsafe_allow_html=True)
+
+
+# LOGIN FLOW
+if st.session_state.login_clicked and not st.session_state.authenticated:
+    password = st.text_input("Wachtwoord", type="password")
+
+    if st.button("Login"):
+        if password == CORRECT_PASSWORD:
+            st.session_state.authenticated = True
+        else:
+            st.error("Verkeerd wachtwoord")
+
+
+# SHOW LIBRARY BUTTON AFTER LOGIN
+if st.session_state.authenticated:
+    st.markdown(f"""
+    <a href="{sheet_link_data}" target="_blank">
+        <button style="padding:10px 20px; font-size:14px;">Open Gamelibrary</button>
+    </a>
+    """, unsafe_allow_html=True)
