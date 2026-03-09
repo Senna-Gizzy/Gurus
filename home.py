fdb75f2359aa151ce67c1d2de63d3a8b927c86f4
Senna-Gizzy <sennabeetje@gmail.com>
2026-03-09 15:12:51 +0100

    Update home.py

--- home.py
+++ home.py
@@ -10,7 +10,7 @@ st.markdown("""
 
 /* Title styling */
 h1 {
-    text-align: center;
+    text-align: right;
     color: white !important;
     font-weight: 700;
 }
@@ -82,7 +82,7 @@ if "login_clicked" not in st.session_state:
     st.session_state.login_clicked = False
 
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col2:
     if st.button("Naar Game Library"):
