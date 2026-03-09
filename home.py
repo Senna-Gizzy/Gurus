8ca5381c375e7644e8673b175b4ead0ce89cd183
Senna-Gizzy <sennabeetje@gmail.com>
2026-03-09 12:31:59 +0100

    Update home.py

--- home.py
+++ home.py
@@ -7,11 +7,10 @@ CORRECT_PASSWORD = st.secrets["guru_password"]
 sheet_link_data = "https://docs.google.com/spreadsheets/d/1Lf9Rs121pEpCPjAhEC7pk00ffvmOim51vLmrUePJP5A/edit#gid=0"
 sheet_link_edit = "https://docs.google.com/spreadsheets/d/10kSJ04j7sNsXuXEinq5sVI_ToM13g8RroU0lSbmic0Y/edit?gid=0#gid=0"
 
-if "authenticated" not in st.session_state:
-    st.session_state.authenticated = False
-
+# Session state defaults
 if "login_clicked" not in st.session_state:
     st.session_state.login_clicked = False
+
 
 col1, col2 = st.columns(2)
 
@@ -27,21 +26,18 @@ with col2:
     """, unsafe_allow_html=True)
 
 
-# LOGIN FLOW
-if st.session_state.login_clicked and not st.session_state.authenticated:
+# Show password only after clicking the button
+if st.session_state.login_clicked:
     password = st.text_input("Wachtwoord", type="password")
 
     if st.button("Login"):
         if password == CORRECT_PASSWORD:
-            st.session_state.authenticated = True
+            st.success("Doorsturen naar Gamelibrary...")
+
+            st.markdown(
+                f'<meta http-equiv="refresh" content="0; url={sheet_link_data}">',
+                unsafe_allow_html=True
+            )
+
         else:
             st.error("Verkeerd wachtwoord")
-
-
-# SHOW LIBRARY BUTTON AFTER LOGIN
-if st.session_state.authenticated:
-    st.markdown(f"""
-    <a href="{sheet_link_data}" target="_blank">
-        <button style="padding:10px 20px; font-size:14px;">Open Gamelibrary</button>
-    </a>
-    """, unsafe_allow_html=True)
